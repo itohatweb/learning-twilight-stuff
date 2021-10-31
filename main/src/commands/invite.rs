eb7594028e3f066fb88193a6c19659a9fbf36fc1
ITOH <to@itoh.at>
2021-10-31 12:37:12 +0100

    move the button builder to util module

--- main/src/commands/invite.rs
+++ main/src/commands/invite.rs
@@ -12,6 +12,7 @@ use twilight_model::{
     id::EmojiId,
 };
 use twilight_util::builder::CallbackDataBuilder;
+use util::builder::ButtonBuilder;
 
 use crate::types::TwHttpClient;
 #[derive(Clone, Debug)]
@@ -66,62 +67,6 @@ impl ComponentBuilder {
 
     fn is_full(&self) -> bool {
         self.0.len() == 5
-    }
-}
-
-#[derive(Clone, Debug)]
-#[must_use = "builders have no effect if unused"]
-struct ButtonBuilder(Button);
-
-impl ButtonBuilder {
-    /// Create a new builder to construct a [`Button`].
-    pub const fn new(style: ButtonStyle, custom_id_or_url: String) -> Self {
-        Self(Button {
-            style,
-            emoji: None,
-            label: None,
-            custom_id: Some(custom_id_or_url),
-            url: None,
-            disabled: false,
-        })
-    }
-
-    /// Consume the builder, returning a [`Button`].
-    #[allow(clippy::missing_const_for_fn)]
-    #[must_use = "builders have no effect if unused"]
-    pub fn build(mut self) -> Button {
-        if self.0.style == ButtonStyle::Link {
-            self.0.url = self.0.custom_id;
-            self.0.custom_id = None;
-        }
-
-        println!("{:#?}", self.0);
-
-        self.0
-    }
-
-    #[allow(clippy::missing_const_for_fn)]
-    #[must_use = "builders have no effect if unused"]
-    pub fn label(mut self, label: String) -> Self {
-        self.0.label = Some(label);
-
-        self
-    }
-
-    #[allow(clippy::missing_const_for_fn)]
-    #[must_use = "builders have no effect if unused"]
-    pub fn emoji(mut self, emoji: ReactionType) -> Self {
-        self.0.emoji = Some(emoji);
-
-        self
-    }
-
-    #[allow(clippy::missing_const_for_fn)]
-    #[must_use = "builders have no effect if unused"]
-    pub fn disabled(mut self, disabled: bool) -> Self {
-        self.0.disabled = disabled;
-
-        self
     }
 }
 
---
+++ util/src/builder/button.rs
@@ -0,0 +1,60 @@
+use twilight_model::{
+    application::component::{button::ButtonStyle, Button},
+    channel::ReactionType,
+};
+
+#[derive(Clone, Debug)]
+#[must_use = "builders have no effect if unused"]
+pub struct ButtonBuilder(Button);
+
+impl ButtonBuilder {
+    /// Create a new builder to construct a [`Button`].
+    pub const fn new(style: ButtonStyle, custom_id_or_url: String) -> Self {
+        Self(Button {
+            style,
+            emoji: None,
+            label: None,
+            custom_id: Some(custom_id_or_url),
+            url: None,
+            disabled: false,
+        })
+    }
+
+    /// Consume the builder, returning a [`Button`].
+    #[allow(clippy::missing_const_for_fn)]
+    #[must_use = "builders have no effect if unused"]
+    pub fn build(mut self) -> Button {
+        if self.0.style == ButtonStyle::Link {
+            self.0.url = self.0.custom_id;
+            self.0.custom_id = None;
+        }
+
+        println!("{:#?}", self.0);
+
+        self.0
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    #[must_use = "builders have no effect if unused"]
+    pub fn label(mut self, label: String) -> Self {
+        self.0.label = Some(label);
+
+        self
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    #[must_use = "builders have no effect if unused"]
+    pub fn emoji(mut self, emoji: ReactionType) -> Self {
+        self.0.emoji = Some(emoji);
+
+        self
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    #[must_use = "builders have no effect if unused"]
+    pub fn disabled(mut self, disabled: bool) -> Self {
+        self.0.disabled = disabled;
+
+        self
+    }
+}
