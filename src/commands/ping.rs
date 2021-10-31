43402c03bd483514155127f1b865af1d863c36df
ITOH <to@itoh.at>
2021-10-31 10:42:45 +0100

    teesting

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -1,16 +1,35 @@
 use anyhow::Result;
-use twilight_model::application::{callback::InteractionResponse, interaction::ApplicationCommand};
+use twilight_model::{
+    application::{callback::InteractionResponse, interaction::ApplicationCommand},
+    id::UserId,
+};
 use twilight_util::builder::CallbackDataBuilder;
+use twilight_util::snowflake::Snowflake;
 
-use crate::types::TwHttpClient;
+use crate::types::{RCache, TwHttpClient};
 
-pub async fn execute(http: TwHttpClient, command: &ApplicationCommand) -> Result<()> {
+pub async fn execute(
+    http: TwHttpClient,
+    command: &ApplicationCommand,
+    cache: RCache,
+) -> Result<()> {
+    // if command.user.as_ref().unwrap().id.0 != UserId::new(615542460151496705_u64).unwrap().0 {
+    //     return Ok(());
+    // }
+
+    let ping_time = chrono::Utc::now().timestamp_millis() - command.id.timestamp();
+
+    let guild_count = cache.read().iter().guilds().count();
+
     http.interaction_callback(
         command.id,
         &command.token,
         &InteractionResponse::ChannelMessageWithSource(
             CallbackDataBuilder::new()
-                .content("foo bar baz".into())
+                .content(format!(
+                    "PONG! Ping time: {}ms\nCached Guilds: {}",
+                    ping_time, guild_count
+                ))
                 .build(),
         ),
     )
