5d3dcfbdea4e16e6ff383e27dabef031e244e808
ITOH <to@itoh.at>
2021-10-31 10:42:38 +0100

    maybe this is not good lets see

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,8 @@
+use parking_lot::RwLock;
 use std::sync::Arc;
+use twilight_cache_inmemory::InMemoryCache;
 use twilight_http::Client as HttpClient;
 
 pub type TwHttpClient = Arc<HttpClient>;
+
+pub type RCache = std::sync::Arc<RwLock<InMemoryCache>>;
