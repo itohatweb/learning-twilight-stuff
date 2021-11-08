a76a8ec526ded00203220dd988a3bd6776ccca2b
ITOH <to@itoh.at>
2021-11-08 21:37:04 +0100

    Update thread.rs

--- cache/src/event/thread.rs
+++ cache/src/event/thread.rs
@@ -1,35 +1,42 @@
-use crate::{config::ResourceType, InMemoryCache, UpdateCache};
+use crate::{config::ResourceType, InRedisCache, UpdateCache};
 use twilight_model::{
     channel::{Channel, GuildChannel},
     gateway::payload::incoming::{ThreadCreate, ThreadDelete, ThreadListSync, ThreadUpdate},
 };
 
+#[async_trait::async_trait]
 impl UpdateCache for ThreadCreate {
-    fn update(&self, cache: &InMemoryCache) {
+    async fn update(&self, cache: &InRedisCache) {
         if !cache.wants(ResourceType::CHANNEL) {
             return;
         }
 
         if let Channel::Guild(c) = &self.0 {
             if let Some(gid) = c.guild_id() {
-                cache.cache_guild_channel(gid, c.clone());
+                cache.cache_guild_channel(gid, c.clone()).await;
             }
         }
     }
 }
 
+#[async_trait::async_trait]
 impl UpdateCache for ThreadDelete {
-    fn update(&self, cache: &InMemoryCache) {
+    async fn update(&self, cache: &InRedisCache) {
         if !cache.wants(ResourceType::CHANNEL) {
             return;
         }
 
-        cache.delete_guild_channel(self.0.id());
+        if let Channel::Guild(c) = &self.0 {
+            if let Some(gid) = c.guild_id() {
+                cache.delete_guild_channel(gid, self.0.id()).await;
+            }
+        }
     }
 }
 
+#[async_trait::async_trait]
 impl UpdateCache for ThreadListSync {
-    fn update(&self, cache: &InMemoryCache) {
+    async fn update(&self, cache: &InRedisCache) {
         if !cache.wants(ResourceType::CHANNEL) {
             return;
         }
@@ -43,19 +50,20 @@ impl UpdateCache for ThreadListSync {
             })
             .collect();
 
-        cache.cache_guild_channels(self.guild_id, threads);
+        cache.cache_guild_channels(self.guild_id, threads).await;
     }
 }
 
+#[async_trait::async_trait]
 impl UpdateCache for ThreadUpdate {
-    fn update(&self, cache: &InMemoryCache) {
+    async fn update(&self, cache: &InRedisCache) {
         if !cache.wants(ResourceType::CHANNEL) {
             return;
         }
 
         if let Channel::Guild(c) = &self.0 {
             if let Some(gid) = c.guild_id() {
-                cache.cache_guild_channel(gid, c.clone());
+                cache.cache_guild_channel(gid, c.clone()).await;
             }
         }
     }
