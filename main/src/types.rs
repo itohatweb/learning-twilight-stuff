c4e4cce9b2a44ebf2b16cafa6dad19445f349b11
ITOH <to@itoh.at>
2021-11-19 16:34:31 +0100

    new context type

--- main/src/types.rs
+++ main/src/types.rs
@@ -1,7 +1,45 @@
 use std::sync::Arc;
-use twilight_cache_inmemory::InMemoryCache;
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
 use twilight_http::Client as HttpClient;
+use twilight_model::application::command::Command as ApplicationCommand;
+use twilight_model::{application::command::CommandOption, id::ApplicationId};
+use util::interaction::responder::InteractionResponder;
 
-pub type TwHttpClient = Arc<HttpClient>;
+// pub type Acache = std::sync::Arc<InMemoryCache>;
 
-pub type Acache = std::sync::Arc<InMemoryCache>;
+pub type Context = Arc<InnerContext>;
+
+pub struct InnerContext {
+    pub http: HttpClient,
+    pub inter: InteractionResponder,
+    pub cache: InMemoryCache,
+}
+
+impl InnerContext {
+    pub fn new(
+        token: String,
+        application_id: ApplicationId,
+        cache_resource_types: ResourceType,
+    ) -> Self {
+        // HTTP is separate from the gateway, so create a new client.
+        let http = HttpClient::new(token.clone());
+
+        // TODO: Interaction HTTP handler
+        let http_inter = HttpClient::new(token);
+        http_inter.set_application_id(application_id);
+
+        let inter = InteractionResponder::new(http_inter);
+
+        let cache = InMemoryCache::builder()
+            .resource_types(cache_resource_types)
+            .build();
+
+        Self { http, inter, cache }
+    }
+}
+
+#[async_trait::async_trait]
+pub trait Command {
+    async fn exec(context: Context, options: Vec<CommandOption>) -> Result<(), String>;
+    fn build() -> ApplicationCommand;
+}
