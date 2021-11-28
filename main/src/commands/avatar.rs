e9f342478fb4a53185aa914a9fcbc501b29e55ca
ITOH <to@itoh.at>
2021-11-28 18:22:09 +0100

    avatar command fixes

--- main/src/commands/avatar.rs
+++ main/src/commands/avatar.rs
@@ -1,6 +1,13 @@
-use twilight_embed_builder::{EmbedBuilder, ImageSource};
-use twilight_model::application::{callback::InteractionResponse, interaction::ApplicationCommand};
-use twilight_util::builder::CallbackDataBuilder;
+use twilight_embed_builder::{EmbedBuilder, EmbedFooterBuilder, ImageSource};
+use twilight_model::application::{
+    callback::InteractionResponse,
+    command::{Command, CommandType},
+    interaction::{application_command::CommandOptionValue, ApplicationCommand},
+};
+use twilight_util::builder::{
+    command::{CommandBuilder, UserBuilder},
+    CallbackDataBuilder,
+};
 use util::{interaction::ApplicationCommandUtils, url::user::UserUtils};
 
 use crate::types::Context;
@@ -8,25 +15,66 @@ use crate::types::Context;
 use super::ExecCommandError;
 
 pub async fn run(context: &Context, command: &ApplicationCommand) -> Result<(), ExecCommandError> {
-    let image_source =
-        ImageSource::url(command.get_user().avatar_url().set_size(4096).build()).unwrap();
-    let _f = context
-        .http
-        .interaction_callback(
+    let user_id = match command.data.options.get(0) {
+        Some(option) => match option.value {
+            CommandOptionValue::User(user) => Some(user),
+            _ => None,
+        },
+        _ => None,
+    };
+
+    let user = match user_id {
+        Some(user_id) => command
+            .data
+            .resolved
+            .as_ref()
+            .map(|r| r.users.iter().find(|u| u.id == user_id))
+            .flatten()
+            .map(|v| v.to_owned()),
+        None => Some(command.get_user()),
+    };
+
+    let user = match user {
+        Some(user) => user,
+        None => {
+            return Err(ExecCommandError::Message(
+                "The user could not be found".into(),
+            ))
+        }
+    };
+
+    let image_source = ImageSource::url(user.avatar_url().set_size(4096).build()).unwrap();
+
+    context
+        .inter
+        .respond(
             command.id,
             &command.token,
             &InteractionResponse::ChannelMessageWithSource(
                 CallbackDataBuilder::new()
                     // .components([component])
                     .embeds(vec![EmbedBuilder::new()
+                        .title("Avatar Link")
+                        .url(user.avatar_url().set_size(1024).build())
                         .image(image_source)
+                        .footer(
+                            EmbedFooterBuilder::new(format!(
+                                "by {}#{}",
+                                command.get_user().name,
+                                command.get_user().discriminator(),
+                            ))
+                            .icon_url(
+                                ImageSource::url(command.get_user().avatar_url().build()).unwrap(),
+                            ),
+                        )
+                        .color(240116)
                         .build()
                         .unwrap()])
                     .build(),
             ),
         )
-        .exec()
-        .await?;
+        .await
+        .unwrap();
 
     Ok(())
 }
